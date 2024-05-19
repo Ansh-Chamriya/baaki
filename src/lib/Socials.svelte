596cbd863247f42b4bda6429c2a0a8bd5f01d115
Ansh Chamriya <AnshChamariya922@gmail.com>
2024-05-19 16:23:53 +0530

    git init

---
+++ src/lib/Socials.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Icon from '@iconify/svelte';
+</script>
+
+<div class="mt-8 flex flex-wrap gap-x-6 gap-y-4 opacity-70">
+	<a href=""><Icon icon="line-md:twitter-x" width="48px" /></a>
+	<Icon icon="line-md:linkedin" width="48px" />
+	<Icon icon="line-md:instagram" width="48px" />
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,212 @@
+<script>
+	import Socials from '$lib/Socials.svelte';
+	import { Linkedin } from 'lucide-svelte';
+</script>
+
+<div class="overflow-hidden py-20 sm:py-32 lg:pb-32 xl:pb-36">
+	<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+		<div class="lg:grid lg:grid-cols-12 lg:gap-x-8 lg:gap-y-20">
+			<div
+				class="relative z-10 mx-auto max-w-2xl lg:col-span-7 lg:max-w-none lg:pt-6 xl:col-span-6"
+			>
+				<h1 class="text-4xl font-medium tracking-tight text-gray-900">
+					Revolutionizing digital credit systems
+				</h1>
+				<p class="mt-6 text-lg text-gray-600">
+					Baaki revolutionizes MSME credit management using UPI QR codes, offering small vendors a
+					seamless solution for real-time, secure financial tracking and enhanced credit processes.
+				</p>
+				<Socials />
+			</div>
+			<div class="relative mt-10 sm:mt-20 lg:col-span-5 lg:row-span-2 lg:mt-0 xl:col-span-6">
+				<div
+					class="absolute left-1/2 top-4 h-[1026px] w-[1026px] -translate-x-1/3 stroke-gray-300/70 sm:top-16 sm:-translate-x-1/2 lg:-top-16 lg:ml-12 xl:-top-14 xl:ml-0"
+				>
+					<svg
+						viewBox="0 0 1026 1026"
+						fill="none"
+						aria-hidden="true"
+						class="animate-spin-slow absolute inset-0 h-full w-full"
+						><path
+							d="M1025 513c0 282.77-229.23 512-512 512S1 795.77 1 513 230.23 1 513 1s512 229.23 512 512Z"
+							stroke="#D4D4D4"
+							stroke-opacity="0.7"
+						></path><path
+							d="M513 1025C230.23 1025 1 795.77 1 513"
+							stroke="url(#:S1:-gradient-1)"
+							stroke-linecap="round"
+						></path><defs
+							><linearGradient
+								id=":S1:-gradient-1"
+								x1="1"
+								y1="513"
+								x2="1"
+								y2="1025"
+								gradientUnits="userSpaceOnUse"
+								><stop stop-color="#06b6d4"></stop><stop
+									offset="1"
+									stop-color="#06b6d4"
+									stop-opacity="0"
+								></stop></linearGradient
+							></defs
+						></svg
+					><svg
+						viewBox="0 0 1026 1026"
+						fill="none"
+						aria-hidden="true"
+						class="animate-spin-reverse-slower absolute inset-0 h-full w-full"
+						><path
+							d="M913 513c0 220.914-179.086 400-400 400S113 733.914 113 513s179.086-400 400-400 400 179.086 400 400Z"
+							stroke="#D4D4D4"
+							stroke-opacity="0.7"
+						></path><path
+							d="M913 513c0 220.914-179.086 400-400 400"
+							stroke="url(#:S1:-gradient-2)"
+							stroke-linecap="round"
+						></path><defs
+							><linearGradient
+								id=":S1:-gradient-2"
+								x1="913"
+								y1="513"
+								x2="913"
+								y2="913"
+								gradientUnits="userSpaceOnUse"
+								><stop stop-color="#06b6d4"></stop><stop
+									offset="1"
+									stop-color="#06b6d4"
+									stop-opacity="0"
+								></stop></linearGradient
+							></defs
+						></svg
+					>
+				</div>
+				<div
+					class="-mx-4 h-[448px] px-9 [mask-image:linear-gradient(to_bottom,white_80%,transparent)] sm:mx-0 lg:absolute lg:-inset-x-10 lg:-bottom-20 lg:-top-10 lg:h-auto lg:px-0 lg:pt-10 xl:-bottom-32"
+				>
+					<div class="relative mx-auto aspect-[366/729] max-w-[366px]">
+						<div
+							class="absolute inset-y-[calc(1/729*100%)] left-[calc(7/729*100%)] right-[calc(5/729*100%)] rounded-[calc(58/366*100%)/calc(58/729*100%)] shadow-2xl"
+						></div>
+						<div
+							class="absolute left-[calc(23/366*100%)] top-[calc(23/729*100%)] grid h-[calc(686/729*100%)] w-[calc(318/366*100%)] transform grid-cols-1 overflow-hidden bg-gray-900 pt-[calc(23/318*100%)]"
+						>
+							<div class="flex flex-col">
+								<div class="flex justify-between px-4 pt-4">
+									<svg viewBox="0 0 24 24" fill="none" aria-hidden="true" class="h-6 w-6 flex-none"
+										><path
+											d="M5 6h14M5 18h14M5 12h14"
+											stroke="#fff"
+											stroke-width="2"
+											stroke-linecap="round"
+											stroke-linejoin="round"
+										></path></svg
+									>
+
+									<svg
+										class="h-6 w-auto flex-none"
+										version="1.0"
+										xmlns="http://www.w3.org/2000/svg"
+										width="2023.000000pt"
+										height="683.000000pt"
+										viewBox="0 0 2023.000000 683.000000"
+										preserveAspectRatio="xMidYMid meet"
+									>
+										<g
+											transform="translate(0.000000,683.000000) scale(0.100000,-0.100000)"
+											fill="#ffffff"
+											stroke="none"
+										>
+											<path
+												d="M19022 6330 c-172 -45 -338 -172 -429 -328 -71 -121 -104 -293 -83
+-433 36 -246 164 -426 380 -534 104 -51 180 -68 305 -68 128 0 203 16 302 67
+180 90 301 229 359 411 105 331 -45 678 -364 839 -130 66 -322 84 -470 46z"
+											/>
+											<path
+												d="M450 3455 l0 -2545 575 0 574 0 3 150 3 149 65 -55 c198 -169 436
+-284 695 -335 108 -22 476 -19 582 4 285 61 540 184 743 357 216 184 364 381
+496 655 133 279 189 535 189 870 -1 346 -61 612 -205 902 -76 154 -145 264
+-230 368 -262 320 -587 522 -981 610 -111 25 -417 31 -544 11 -286 -44 -555
+-170 -765 -360 l-45 -41 -3 903 -2 902 -575 0 -575 0 0 -2545z m2080 105 c541
+-86 844 -706 607 -1241 -129 -292 -411 -478 -724 -479 -117 0 -180 11 -276 50
+-104 43 -173 88 -259 169 -385 364 -357 1014 57 1338 179 140 383 196 595 163z"
+											/>
+											<path
+												d="M14330 3455 l0 -2545 580 0 580 0 0 707 0 706 20 -18 c11 -10 298
+-328 638 -707 l617 -688 694 0 c660 0 693 1 679 18 -37 43 -1385 1582 -1491
+1701 l-118 134 24 26 c13 14 145 166 293 336 864 995 1184 1365 1184 1370 0 3
+-312 5 -692 4 l-693 0 -575 -680 -575 -680 -3 1431 -2 1430 -580 0 -580 0 0
+-2545z"
+											/>
+											<path
+												d="M6380 4603 c-137 -14 -373 -84 -502 -149 -481 -242 -831 -687 -969
+-1233 -42 -164 -59 -316 -59 -519 0 -260 28 -437 101 -657 161 -485 508 -889
+941 -1095 220 -104 373 -139 639 -147 233 -7 339 7 520 67 189 63 376 169 512
+290 l57 52 0 -151 0 -151 580 0 580 0 0 1795 0 1795 -580 0 -580 0 0 -152 0
+-152 -37 36 c-193 183 -473 315 -768 363 -82 14 -337 18 -435 8z m621 -1058
+c138 -36 262 -110 369 -219 177 -181 259 -404 247 -668 -8 -181 -57 -328 -159
+-474 -150 -217 -388 -344 -648 -344 -295 0 -560 165 -702 436 -126 242 -137
+539 -29 792 105 249 335 439 587 487 95 18 245 13 335 -10z"
+											/>
+											<path
+												d="M11005 4603 c-80 -10 -237 -48 -328 -79 -338 -117 -676 -389 -887
+-714 -107 -165 -225 -451 -265 -639 -69 -329 -64 -689 15 -996 32 -126 68
+-220 135 -360 97 -201 198 -347 344 -499 243 -250 518 -409 836 -481 162 -37
+448 -47 607 -20 262 43 527 168 726 342 l62 55 0 -151 0 -151 580 0 580 0 0
+1795 0 1795 -580 0 -579 0 -3 -149 -3 -149 -69 59 c-214 182 -462 293 -756
+339 -61 9 -348 12 -415 3z m570 -1048 c330 -58 597 -337 661 -690 95 -533
+-286 -1025 -795 -1025 -297 0 -558 162 -703 436 -115 219 -135 482 -56 721 95
+287 338 507 615 557 104 19 173 19 278 1z"
+											/>
+											<path
+												d="M18620 2705 l0 -1795 580 0 580 0 0 1795 0 1795 -580 0 -580 0 0
+-1795z"
+											/>
+										</g>
+									</svg>
+
+									<svg viewBox="0 0 24 24" fill="none" aria-hidden="true" class="h-6 w-6 flex-none"
+										><path
+											d="M15 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM6.696 19h10.608c1.175 0 2.08-.935 1.532-1.897C18.028 15.69 16.187 14 12 14s-6.028 1.689-6.836 3.103C4.616 18.065 5.521 19 6.696 19Z"
+											stroke="#fff"
+											stroke-width="2"
+											stroke-linecap="round"
+											stroke-linejoin="round"
+										></path></svg
+									>
+								</div>
+								<div class="mt-4 flex-auto rounded-t-2xl bg-white">
+									<div class=" flex items-center justify-center p-4">
+										<img src="/comingSoon.gif" alt="" class="  invert" />
+									</div>
+								</div>
+							</div>
+						</div>
+						<svg
+							viewBox="0 0 366 729"
+							aria-hidden="true"
+							class="pointer-events-none absolute inset-0 h-full w-full fill-gray-100"
+							><path
+								fill="#F2F2F2"
+								fill-rule="evenodd"
+								clip-rule="evenodd"
+								d="M300.092 1c41.22 0 63.223 21.99 63.223 63.213V184.94c-.173.184-.329.476-.458.851.188-.282.404-.547.647-.791.844-.073 2.496.257 2.496 2.157V268.719c-.406 2.023-2.605 2.023-2.605 2.023a7.119 7.119 0 0 1-.08-.102v394.462c0 41.213-22.001 63.212-63.223 63.212h-95.074c-.881-.468-2.474-.795-4.323-.838l-33.704-.005-.049.001h-.231l-.141-.001c-2.028 0-3.798.339-4.745.843H66.751c-41.223 0-63.223-21.995-63.223-63.208V287.739c-.402-.024-2.165-.23-2.524-2.02v-.973A2.039 2.039 0 0 1 1 284.62v-47.611c0-.042.001-.084.004-.126v-.726c0-1.9 1.652-2.23 2.496-2.157l.028.028v-16.289c-.402-.024-2.165-.23-2.524-2.02v-.973A2.039 2.039 0 0 1 1 214.62v-47.611c0-.042.001-.084.004-.126v-.726c0-1.9 1.652-2.23 2.496-2.157l.028.028v-26.041a2.26 2.26 0 0 0 .093-.236l-.064-.01a3.337 3.337 0 0 1-.72-.12l-.166-.028A2 2 0 0 1 1 135.62v-24.611a2 2 0 0 1 1.671-1.973l.857-.143v-44.68C3.528 22.99 25.53 1 66.75 1h233.341ZM3.952 234.516a5.481 5.481 0 0 0-.229-.278c.082.071.159.163.228.278Zm89.99-206.304A4.213 4.213 0 0 0 89.727 24H56.864C38.714 24 24 38.708 24 56.852v618.296C24 693.292 38.714 708 56.864 708h250.272c18.15 0 32.864-14.708 32.864-32.852V56.852C340 38.708 325.286 24 307.136 24h-32.864a4.212 4.212 0 0 0-4.213 4.212v2.527c0 10.235-8.3 18.532-18.539 18.532H112.48c-10.239 0-18.539-8.297-18.539-18.532v-2.527Z"
+							></path><rect x="154" y="29" width="56" height="5" rx="2.5" fill="#D4D4D4"
+							></rect></svg
+						>
+						<img
+							alt=""
+							fetchpriority="high"
+							width="366"
+							height="729"
+							decoding="async"
+							data-nimg="1"
+							class="pointer-events-none absolute inset-0 h-full w-full"
+							style="color:transparent"
+							src="/phone-frame.d4b6b62a.svg"
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
